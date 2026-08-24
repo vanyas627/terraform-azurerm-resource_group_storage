c5e1c61b8a5923ce5bb0898155251767a45fb8dc
vanyas627 <vanja.sotsenko@gmail.com>
2026-08-24 17:14:50 +0200

    Resource_storage_module_commit

---
+++ modules/resource_group_storage/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  description = "This a name of resource group"
+  default = "terraform_task_6"
+}
+
+variable "storage_account_name" {
+  description = "This a name of storage account"
+  default = "vanyas627storage"
+}
+
+variable "location" {
+  description = "Location where resources will be created"
+  default = "West Europe"
+}
