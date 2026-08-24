8b8fa62e5a65735a82e3f812441a19dc680e83ef
vanyas627 <vanja.sotsenko@gmail.com>
2026-08-24 18:04:44 +0200

    change README.md

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "resource_group_name" {
   description = "This a name of resource group"
-  default = "terraform_task_6"
+  default     = "terraform_task_6"
 }
 
 variable "storage_account_name" {
   description = "This a name of storage account"
-  default = "vanyas627storage"
+  default     = "vanyas627storage"
 }
 
 variable "location" {
   description = "Location where resources will be created"
-  default = "West Europe"
+  default     = "West Europe"
 }
